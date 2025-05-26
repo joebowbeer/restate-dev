ac7165f153ecd5c25e6a81df8c84eab2ce5a5ab8
Joe Bowbeer <joe.bowbeer@gmail.com>
2025-05-26 16:09:52 -0700

    Add restatedev NodeJS template

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import * as restate from "@restatedev/restate-sdk";
+import { sendNotification, sendReminder } from "./utils";
+
+restate
+  .endpoint()
+  .bind(
+    restate.service({
+      name: "Greeter",
+      handlers: {
+        greet: async (ctx: restate.Context, name: string) => {
+          // Durably execute a set of steps; resilient against failures
+          const greetingId = ctx.rand.uuidv4();
+          await ctx.run(() => sendNotification(greetingId, name));
+          await ctx.sleep(1000);
+          await ctx.run(() => sendReminder(greetingId));
+
+          // Respond to caller
+          return `You said hi to ${name}!`;
+        },
+      },
+    }),
+  )
+  .listen(9080);
